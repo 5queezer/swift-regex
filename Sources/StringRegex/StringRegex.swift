6017b38ecb89aedfd6f2c5e190a0795cce97695a
Christian Pojoni <34570565+5queezer@users.noreply.github.com>
2022-08-06 23:59:00 +0200

    🚀

--- Sources/StringRegex/StringRegex.swift
+++ Sources/StringRegex/StringRegex.swift
@@ -1,6 +1,74 @@
-public struct StringRegex {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+extension String {
+    /// <#Description#>
+    /// - Parameter regex: <#regex description#>
+    /// - Returns: <#description#>
+    func matches(regex: String) -> [String] {
+        let range = NSRange(location: 0, length: self.utf16.count)
+            let regex = try! NSRegularExpression(pattern: regex)
+        let matches = regex.matches(in: self, range: range)
+        
+        let result: [String]? = matches.map { m in
+            (self as NSString).substring(with: m.range(at: 0))
+        }
+        return result!
     }
+    
+    func matchUnnamedGroups(regex: String) -> [String]? {
+        let range = NSRange(location: 0, length: self.utf16.count)
+            let regex = try! NSRegularExpression(pattern: regex)
+        let matches = regex.matches(in: self, range: range)
+        
+        guard let first = matches.first else {
+            return []
+        }
+        
+        guard first.numberOfRanges == 1 else {
+            var names: [String] = []
+            for match in matches {
+                for rangeIndex in 1..<match.numberOfRanges {
+                    let matchRange = match.range(at: rangeIndex)
+                    if matchRange == range { continue }
+                    if let substringRange = Range(matchRange, in: self) {
+                        let capture = String(self[substringRange])
+                        names.append(capture)
+                    }
+                }
+            }
+            
+            return names
+        }
+
+        return nil
+    }
+    
+    /// Gets named capture groups
+    /// - Parameter regex: a regex expression
+    /// - Returns: Dictionary with group name as key and its value
+    func matchNamedGroups(regex: String) -> [String: String]? {
+        guard let groups = regex.matchUnnamedGroups(regex: "\\(\\?<(\\w+)>") else {
+            return nil
+        }
+
+        let range = NSRange(location: 0, length: self.utf16.count)
+        let regex = try! NSRegularExpression(pattern: regex)
+        
+        guard let match = regex.matches(in: self, range: range).first else {
+            return nil
+        }
+        
+        var result: [String: String] = [:]
+        
+        for group in groups {
+            let matchRange = match.range(withName: group)
+            if let substringRange = Range(matchRange, in: self) {
+                let capture = String(self[substringRange])
+                result[group] = capture
+            }
+        }
+        
+        return result
+    }
+    
 }
--- Tests/StringRegexTests/StringRegexTests.swift
+++ Tests/StringRegexTests/StringRegexTests.swift
@@ -2,10 +2,66 @@ import XCTest
 @testable import StringRegex
 
 final class StringRegexTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(StringRegex().text, "Hello, World!")
+    func testEmpty() {
+        let s = ""
+        let regex = "\\w"
+        let matches = s.matches(regex: regex)
+        XCTAssertEqual(matches.isEmpty, true)
+    }
+    
+    func testRegexMultiple() {
+        let s = "Hello World!"
+        let regex = "\\w+"
+        let matches = s.matches(regex: regex)
+        XCTAssertEqual(matches, ["Hello", "World"])
+    }
+    
+    func testRegexMixed() {
+        let s = "Hello 2 World! 123 456."
+        let regex = "[[:alpha:]]+|[[:digit:]]+"
+        let matches = s.matches(regex: regex)
+        XCTAssertEqual(matches, ["Hello", "2", "World", "123", "456"])
+    }
+    
+    func testRegexUnamedGroups() {
+        let s = "The quick brown fox jumps over the lazy dog."
+        let regex = "(fox).*(dog).*"
+        let matches = s.matchUnnamedGroups(regex: regex)
+        XCTAssertEqual(matches, ["fox", "dog"])
+    }
+    
+    func testRegexUnamedGroupsTokens() {
+        let s = "The quick brown fox jumps over the lazy dog."
+        let regex = "(\\w+)"
+        let matches = s.matchUnnamedGroups(regex: regex)
+        XCTAssertEqual(matches, ["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"])
+    }
+    
+    func testGroupsEmpty() {
+        let s = ""
+        let regex = "\\w"
+        let matches = s.matches(regex: regex)
+        XCTAssertEqual(matches.isEmpty, true)
+    }
+    
+    func testRegexEmptyNamedGroups() {
+        let s = ""
+        let regex = "(?<forname>John)\\s*(?<surname>Doe)"
+        let matches = s.matchNamedGroups(regex: regex)
+        XCTAssertNil(matches)
+    }
+    
+    func testRegexNamedGroups() {
+        let s = "John Doe"
+        let regex = "(?<forname>\\w+)\\s+(?<surname>\\w+)"
+        let matches = s.matchNamedGroups(regex: regex)
+        XCTAssertEqual(matches!, ["forname": "John", "surname": "Doe"])
+    }
+    
+    func testRegexMixedUnamedNamedGroups() {
+        let s = "John Peter Doe"
+        let regex = "(?<forename>\\w+)\\s+(\\w+)\\s+(?<surname>\\w+)"
+        let matches = s.matchNamedGroups(regex: regex)
+        XCTAssertEqual(matches!, ["forename": "John", "surname": "Doe"])
     }
 }
